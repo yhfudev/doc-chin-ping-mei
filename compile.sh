500e024b9634217bec7714c9d2c99e0068242c47
yhfudev <yhfudev@gmail.com>
2014-12-27 22:14:46 -0500

    add {showcontents}

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 
 
 
-if [ 0 = 1 ]; then
+if [ 1 = 1 ]; then
 make SOURCE=jpm-xiuxiang.tex TARGET=jpmxx-eink.pdf clean
 
 # create PDF file for e-ink device: no margin, small size, etc.
@@ -20,7 +20,7 @@ make LTARGS="\def\usecjk{1} \def\pagewidth{8in} \def\pageheight{11in} \def\fnote
 
 fi
 
-if [ 0 = 1 ]; then
+if [ 1 = 1 ]; then
 rm -f test-eimd.pdf
 make LTARGS="\def\usecjk{1} \def\pagewidth{4in} \def\pageheight{5.4in} \def\fnotemode{modern} \def\clinemode{horizontal} \def\pageorien{portrait} \def\margin{0.1in} \def\background{white}" SOURCE=test.tex TARGET=test-eimd.pdf test-eimd.pdf
 
@@ -35,12 +35,11 @@ fi
 
 
 # add "showcontents" to all .tex files
-CNT=21
-while (( $CNT < 101 )); do
-    V=$(echo | awk -v i=${CNT} '{printf("%03d", i)}')
-    FN_TEX="chap-cihua/jpm.$V.tex"
-    sed -e 's|\(chapter{.*\)$|\1\n\n\n\\begin{showcontents}{}\n\n|g' "${FN_TEX}"
-    echo -e "\n\n\n\n\\\\end{showcontents}\n\n" >> "${FN_TEX}"
-done
-
-make LTARGS="\def\usecjk{1} \def\pagewidth{8in} \def\pageheight{11in} \def\fnotemode{gezhu} \def\clinemode{vertical} \def\pageorien{portrait} \def\margin{0.8in} \def\background{white}" SOURCE=jpm-cihua.tex TARGET=jpmch-zhgz.pdf jpmch-zhgz.pdf
+#CNT=1
+#while (( $CNT < 101 )); do
+    #V=$(echo | awk -v i=${CNT} '{printf("%03d", i)}')
+    #FN_TEX="chap-cihua/jpm.$V.tex"
+    #sed -i -e 's|\(chapter{.*\)$|\1\n\n\n\\begin{showcontents}{}\n\n|g' "${FN_TEX}"
+    #echo -e "\n\n\n\n\\\\end{showcontents}\n\n" >> "${FN_TEX}"
+    #CNT=$(( $CNT + 1 ))
+#done
